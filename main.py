3e729022fa694b98ee024ea08c5b559086784deb
Brendan <brendan.rogerz@gmail.com>
2019-08-28 06:11:10 +1000

    added validation, though it doesnt work

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ pos, neg = data.build_data.get_data()
 
 max_l = max([len(i.split(' ')) for i in pos+neg])
 
-pos_vec = util.vec_data(pos, lut, max_l, [1, 0])
-neg_vec = util.vec_data(neg, lut, max_l, [0, 1])
+pos_vec = util.vec_data(pos, lut, max_l, 0)
+neg_vec = util.vec_data(neg, lut, max_l, 1)
 
 
 data = neg_vec+pos_vec
@@ -27,22 +27,59 @@ random.shuffle(data)
 percent_split = int(len(data)*0.1)
 train_data, train_target = util.separate_target(data[:-percent_split])
 valid_data, valid_target = util.separate_target(data[-percent_split:])
+
 train_data, train_target = util.batchify(train_data, train_target, batch_size)
-train_data = torch.Tensor(train_data)
-train_target = torch.Tensor(train_target).long()
+valid_data, valid_target = util.batchify(valid_data, valid_target, batch_size)
+
+train_data = torch.Tensor(train_data).cuda()
+train_target = torch.Tensor(train_target).long().cuda()
+
+valid_data = torch.Tensor(valid_data).cuda()
+valid_target = torch.Tensor(valid_target).long().cuda()
+
 train_data = train_data[:,:,None,:]
+valid_data = valid_data[:,:,None,:]
 
 cnn = model.Net()
 cnn.cuda()
-train_data = train_data.cuda()
-train_target = train_target.cuda()
+
 #output = cnn(train_data[0])
-#print(output)
+print("Batches: ", train_data.size()[0])
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(cnn.parameters(), lr=0.001, momentum=0.9)
 
-epochs = 10
+params = list(cnn.parameters()) + list(criterion.parameters())
+total_params = sum(x.size()[0] * x.size()[1] if len(x.size()) > 1 else x.size()[0] for x in params if x.size())
+print('Model total parameters:', total_params)
+
+def evaluate(net: model.Net):
+    net.eval()
+    corrects, avg_loss = 0, 0
+    for batch in range(valid_data.size()[0]):
+        with torch.no_grad():
+            inputs = valid_data[batch]
+            targets = valid_target[batch]
+
+            logit = net(inputs)
+            loss = nn.CrossEntropyLoss()(logit, targets, size_average=False)
+
+            avg_loss += loss
+            #corrects += (torch.max(logit, 1)[1].view(targets.size()) == target.data).sum()
+
+    size = len(valid_data.size()[0]*valid_data[1])
+    avg_loss /= size
+    #accuracy = 100.0 * corrects / size
+    print('\nEvaluation - loss: {:.6f}  acc: {:.4f}%({}/{}) \n'.format(avg_loss,
+                                                                       0,
+                                                                       corrects,
+                                                                       0))
+    return avg_loss
+
+
+
+epochs = 100
+print("Started training")
 for e in range(epochs):
 
     running_loss = 0.0
@@ -52,14 +89,17 @@ for e in range(epochs):
 
         inputs = train_data[batch]
         targets = train_target[batch]
-        print(targets.size(), targets)
+        #print(targets.size(), targets)
         outputs = cnn(inputs)
         loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
 
         running_loss += loss.item()
-        if batch % 100 == 0:  # print every 2000 mini-batches
+        if batch == 190 :
             print('[%d, %5d] loss: %.3f' %
-                  (e + 1, batch + 1, running_loss / 100))
+                  (e + 1, batch + 1, running_loss / 190))
             running_loss = 0.0
+    #evaluate(cnn)
+
+
